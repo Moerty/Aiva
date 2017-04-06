2c90e05ac4d9090da26c06b36e34346f174b0849
Moerty <git@moerty.de>
2017-04-06 20:16:58 +0200

    twitch auth

--- Client/TwitchAuthentication.cs
+++ Client/TwitchAuthentication.cs
@@ -10,34 +10,53 @@ using System.Web;
 
 namespace Client {
     public class TwitchAuthentication {
-        HttpListener listener = new HttpListener();
-        string prefix = "http://localhost:12345/";
-
-
-
         private static readonly TwitchAuthentication instance = new TwitchAuthentication();
-
         public static TwitchAuthentication Instance {
             get {
                 return instance;
             }
         }
 
+        HttpListener listener = new HttpListener();
+        string prefix = "http://localhost:12345/";
+
         private TwitchAuthentication() {
             listener.Prefixes.Add(prefix);
         }
 
+        /// <summary>
+        /// Get Twitch Authentication Async
+        /// </summary>
+        /// <returns></returns>
         public async Task<AuthenticationModel> GetAuthenticationValuesAsync() {
             return await Task.Run(() => GetAuthenticationValues());
         }
 
-        public AuthenticationModel GetAuthenticationValues() {
+        /// <summary>
+        /// Start the listener
+        /// </summary>
+        /// <returns>returns IsListening Value</returns>
+        private bool StartListener() {
             try {
                 listener.Start();
             }
-            catch(HttpListenerException ex) {
-                return null;
+            catch (HttpListenerException ex) {
+                throw new Exception("Cant start listener for TwitchAuthentication");
             }
+
+            return listener.IsListening;
+        }
+
+        /// <summary>
+        /// Stop the listener
+        /// </summary>
+        private void StopListener() {
+            listener.Stop();
+        }
+
+        public AuthenticationModel GetAuthenticationValues() {
+
+            StartListener();
 
             AuthenticationModel Values = null;
             while(listener.IsListening) {
@@ -50,13 +69,12 @@ namespace Client {
                         string access_token = HttpUtility.ParseQueryString(myUri.Query).Get(0).Replace("access_token=", "");
 
                         if(!String.IsNullOrEmpty(scope) && !String.IsNullOrEmpty(access_token)) {
-                            //listener.Stop();
                             Values = GetModel(access_token, scope);
                         }
                     }
                 }
 
-                byte[] b = Encoding.UTF8.GetBytes(File.ReadAllText(@"D:\kill\ConsoleApp1\token.html"));
+                byte[] b = Encoding.UTF8.GetBytes(GetResponse());
                 context.Response.StatusCode = 200;
                 context.Response.KeepAlive = false;
                 context.Response.ContentLength64 = b.Length;
@@ -66,6 +84,7 @@ namespace Client {
                 context.Response.Close();
 
                 if(Values != null) {
+                    StopListener();
                     return Values;
                 }
 
@@ -74,6 +93,44 @@ namespace Client {
             return null;
         }
 
+        /// <summary>
+        /// Creates the Response for TwitchOAuth
+        /// </summary>
+        /// <returns>Response</returns>
+        private string GetResponse() {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("<html>");
+            builder.Append(Environment.NewLine);
+            builder.Append("<head>");
+            builder.Append(Environment.NewLine);
+            builder.Append("<title>BlackBoxBot Twitch Oauth</title>");
+            builder.Append(Environment.NewLine);
+            builder.Append("<script language=\"JavaScript\">");
+            builder.Append(Environment.NewLine);
+            builder.Append("if(window.location.hash) {");
+            builder.Append(Environment.NewLine);
+            builder.Append("window.location.href = window.location.href.replace(\"/#\",\"?=\");");
+            builder.Append(Environment.NewLine);
+            builder.Append("}");
+            builder.Append(Environment.NewLine);
+            builder.Append("</script>");
+            builder.Append(Environment.NewLine);
+            builder.Append("</head>");
+            builder.Append(Environment.NewLine);
+            builder.Append("<body>You can close this tab</body>");
+            builder.Append(Environment.NewLine);
+            builder.Append("</html>");
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Creates the Model to return
+        /// </summary>
+        /// <param name="token">Twitch Token</param>
+        /// <param name="scopes">Twitch Scopes</param>
+        /// <returns></returns>
         private TwitchAuthentication.AuthenticationModel GetModel(string token, string scopes) {
             return new TwitchAuthentication.AuthenticationModel {
                 Token = token,
@@ -81,17 +138,22 @@ namespace Client {
             };
         }
 
+        /// <summary>
+        /// Starts the Request for the User to authorize for twitch
+        /// </summary>
         public void SendRequestToBrowser() {
             Thread.Sleep(500);
 
             string urlS = getUrl();
             Uri uri = new Uri(urlS);
-            //Uri.es
 
-            //System.Diagnostics.Process.Start((Uri.EscapeDataString(urlS)));
             System.Diagnostics.Process.Start(urlS);
         }
 
+        /// <summary>
+        /// Returns the URL which we call to create a oauth token
+        /// </summary>
+        /// <returns></returns>
         private static string getUrl() {
             var sb = new StringBuilder();
             sb.Append("https://api.twitch.tv/kraken/oauth2/authorize");
@@ -102,8 +164,9 @@ namespace Client {
             return sb.ToString();
         }
 
-
-
+        /// <summary>
+        /// Model
+        /// </summary>
         public class AuthenticationModel {
             public string Token { get; set; }
             public string Scopes { get; set; }
