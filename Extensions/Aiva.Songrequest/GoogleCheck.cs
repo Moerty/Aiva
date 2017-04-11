74bce88b2001e08b096f7d62d3d9ee826d48f2d1
Moerty <git@moerty.de>
2017-04-11 21:16:29 +0200

    .

--- Extensions/Aiva.Songrequest/GoogleCheck.cs
+++ Extensions/Aiva.Songrequest/GoogleCheck.cs
@@ -14,13 +14,12 @@ namespace Songrequest {
 			var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets
 			{
 				ClientId = "58340406586-ncdbbvr3p49gisgbjk5uu0vtfdfm5gsn.apps.googleusercontent.com",
-				ClientSecret = "S0efx9ay11SH3Mly5HJ3zwVn"
+                ClientSecret = "S0efx9ay11SH3Mly5HJ3zwVn"
             },
 			new[] { YouTubeService.Scope.Youtube },
 			"user",
 			CancellationToken.None,
 			new FileDataStore("AivaBot")).Result;
-
 
 			var youtubeService = new YouTubeService(new BaseClientService.Initializer()
 			{
@@ -30,19 +29,5 @@ namespace Songrequest {
 
             return credential != null;
 		}
-
-		public static void GetToken()
-		{
-			var client = new RestClient("https://accounts.google.com/o/oauth2/auth");
-			RestRequest request = new RestRequest() { Method = Method.POST };
-
-			request.AddHeader("redirect_uri", "http://localhost");
-			request.AddHeader("response_type", "token");
-			request.AddHeader("scope", "https://www.googleapis.com/auth/youtube");
-			request.AddHeader("client_id", "client_id=235778313506-9i7s8ghgtgn9v767murqk740i7febtho.apps.googleusercontent.com");
-
-			var result = client.Execute(request);
-		}
-
     }
 }
