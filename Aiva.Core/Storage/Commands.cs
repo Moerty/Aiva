f70ac6e008bf810b794431cafffe341a48977705
Moerty <git@moerty.de>
2017-11-08 01:59:12 +0100

    change commands table

--- Aiva.Core/Storage/Commands.cs
+++ Aiva.Core/Storage/Commands.cs
@@ -14,14 +14,13 @@ namespace Aiva.Core.Storage
     
     public partial class Commands
     {
-        public long Index { get; set; }
-        public string Command { get; set; }
-        public string Text { get; set; }
-        public System.DateTime CreatedAt { get; set; }
-        public Nullable<System.DateTime> ModifiedAt { get; set; }
-        public long ExecutionRight { get; set; }
+        public long ID { get; set; }
+        public Nullable<long> Name { get; set; }
+        public long Text { get; set; }
+        public long CreatedAt { get; set; }
+        public Nullable<long> ModifiedAt { get; set; }
+        public long Interval { get; set; }
+        public Nullable<long> NextExecution { get; set; }
         public Nullable<long> Count { get; set; }
-        public Nullable<long> Cooldown { get; set; }
-        public Nullable<System.DateTime> LastExecution { get; set; }
     }
 }
